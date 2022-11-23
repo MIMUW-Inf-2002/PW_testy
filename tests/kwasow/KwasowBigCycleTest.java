e3468136684f21255585fe3f89b62a280d70826a
Karol Wąsowski <wasowski02@protonmail.com>
2022-11-23 09:36:52 +0100

    Added bigger cycle test

--- tests/kwasow/KwasowBigCycleTest.java
+++ tests/kwasow/KwasowBigCycleTest.java
@@ -7,27 +7,32 @@ import cp2022.tests.kwasow.shared.KwasowWorkplace;
 import cp2022.tests.kwasow.shared.KwasowWorkplaceId;
 
 import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
 
 import static cp2022.tests.kwasow.shared.KwasowLogger.log;
 
-public class KwasowPairCycleTest {
+public class KwasowBigCycleTest {
 
   public static void run() {
     run(false);
   }
 
   public static void run(boolean verbose) {
-    System.out.println("\nRunning test: " + KwasowPairCycleTest.class.getSimpleName());
+    System.out.println("\nRunning test: " + KwasowBigCycleTest.class.getSimpleName());
 
     KwasowWorkplace saw = new KwasowWorkplace(new KwasowWorkplaceId(0), "the saw", verbose);
     KwasowWorkplace hammer = new KwasowWorkplace(new KwasowWorkplaceId(1), "the hammer", verbose);
+    KwasowWorkplace sink = new KwasowWorkplace(new KwasowWorkplaceId(2), "the sink", verbose);
 
-    ArrayList<Workplace> workplaces = new ArrayList<>(2);
+    ArrayList<Workplace> workplaces = new ArrayList<>(3);
     workplaces.add(saw);
     workplaces.add(hammer);
+    workplaces.add(sink);
 
     Workshop workshop = WorkshopFactory.newWorkshop(workplaces);
-    ArrayList<Thread> threads = new ArrayList<>(2);
+    ArrayList<Thread> threads = new ArrayList<>(4);
+
+    CountDownLatch syncLatch = new CountDownLatch(3);
 
     Runnable worker = () -> {
       try {
@@ -39,16 +44,23 @@ public class KwasowPairCycleTest {
         log(myName + " now occupies " + saw.getName(), verbose);
         workplace.use();
 
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 3; i++) {
           log(myName + " tries to switch its workplace to " + hammer.getName(), verbose);
           workplace = workshop.switchTo(hammer.getId());
           log(myName + " now occupies " + hammer.getName(), verbose);
+          workplace.use();
+
+          log(myName + " tries to switch its workplace to " + sink.getName(), verbose);
+          workplace = workshop.switchTo(sink.getId());
+          log(myName + " now occupies " + sink.getName(), verbose);
           workplace.use();
 
           log(myName + " tries to switch its workplace to " + saw.getName(), verbose);
           workplace = workshop.switchTo(saw.getId());
           log(myName + " now occupies " + saw.getName(), verbose);
           workplace.use();
+
+          syncLatch.countDown();
         }
 
         log(myName + " leaves the workshop", verbose);
@@ -62,8 +74,28 @@ public class KwasowPairCycleTest {
       }
     };
 
+    Runnable seba = () -> {
+      try {
+        // Seba czeka, aż wszystkie miejsca będą zajęte i wszystko się zacykli
+        syncLatch.await();
+        String myName = Thread.currentThread().getName();
+
+        log(myName + " tries to enter the workshop and occupy " + saw.getName(), verbose);
+        Workplace workplace = workshop.enter(saw.getId());
+        log(myName + " now occupies " + saw.getName(), verbose);
+        workplace.use();
+
+        log(myName + " leaves the workshop", verbose);
+        workshop.leave();
+      } catch (InterruptedException e) {
+        System.out.println("The " + KwasowPairCycleTest.class.getSimpleName() + " test was interrupted");
+      }
+    };
+
     threads.add(new Thread(worker, "Brajan"));
     threads.add(new Thread(worker, "Dżesika"));
+    threads.add(new Thread(worker, "Janusz"));
+    threads.add(new Thread(seba, "Seba"));
 
     for (Thread t : threads) {
       t.start();
@@ -79,4 +111,3 @@ public class KwasowPairCycleTest {
   }
 
 }
-
--- tests/kwasow/KwasowMain.java
+++ tests/kwasow/KwasowMain.java
@@ -3,10 +3,21 @@ package cp2022.tests.kwasow;
 public class KwasowMain {
 
   public static void main(String[] args) {
+    // Ustaw tę wartość na true, jeśli chcesz otrzymywać takie same logi jak
+    // w oficjalnym przykładzie
     boolean verbose = false;
-    System.out.println("\nRunning Kwasow tests with verbose set to " + verbose);
 
+    // Ten test ma dwóch robotników, którzy na zmianę używają dwóch narzędzi
+    // i przesiadają się między sobą
     KwasowPairCycleTest.run(verbose);
+
+    // Ten test ma czterech robotników i trzy stanowiska. Trzech z nich przesiada
+    // się w cyklu, a czwarty (Seba) próbuje im się wbić w ten cykl w pewnym
+    // momencie (dokładniej to wtedy, kiedy wykonają jedno pełne przesiadanie
+    // w cyklu chyba, aczkolwiek tu oczywiści może się zdarzyć jakiś dziwny
+    // przeplot, który sprawi, że Seba przyjdzie dopiero, jak cała trójka
+    // sobie już pójdzie).
+    KwasowBigCycleTest.run(verbose);
   }
 
 }
