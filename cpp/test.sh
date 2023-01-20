e371645afed21d835351d1e2c2bad0963ba9d225
Anh Dinh Trong <ad438410@students.mimuw.edu.pl>
2023-01-20 17:24:25 +0100

    Add makeshift shell script

---
+++ cpp/test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+[ -f './system.cpp' -a -f './system.hpp' ] ||
+    { echo "Nie znalazłem plików."; exit 1; }
+
+cp -v ./system.cpp ./system.hpp demo
+cd demo
+make
+LD_LIBRARY_PATH=/opt/gcc-11.1/lib64 ./main
+rm main
